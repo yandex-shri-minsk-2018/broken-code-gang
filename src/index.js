f7f7e52b8cd781bbf21dfbf83fd49bc5a9822bc2
Igor Zayac <zihor1980@gmail.com>
2018-03-31 18:02:40 +0300

    Reducers: Add actions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App/App';
-import rootReducer from './reducers'
+// import rootReducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 // import './components/Header/Header.css';
 import reducer from './reducers/chats';
 
 
 import api from './api';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //
 // Example of usage API
 //
 (async () => {
-   //
-   // Events
-   //
+  //
+  // Events
+  //
 
    // On status of user is changed
    await api.onUserChangeStatus((result) => {
@@ -35,66 +36,87 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
        console.log('User leaved room: ', result);
    });
 
-   // On user is joined to room
-   await api.onMessage((result) => {
-       console.log('New message: ', result);
-   });
+    // On user is joined to room
+    await api.onMessage((result) => {
+        console.log('New message: ', result);
+    });
+  // On status of user is changed
+  await api.onUserChangeStatus((result) => {
+    console.log('Change status: ', result);
+  });
 
-   //
-   // Actions
-   //
+  // On user is joined to room
+  await api.onUserJoinedRoom((result) => {
+    console.log('User joined room: ', result);
+  });
 
-   // Fetch current user
-   let user = await api.getCurrentUser();
-   console.log('Current user', user);
+  // On user is joined to room
+  await api.onUserLeavedRoom((result) => {
+    console.log('User leaved room: ', result);
+  });
 
-   // Fetch user information
-   console.log('User information', await api.getUser(user._id));
+  // On user is joined to room
+  await api.onMessage((result) => {
+    console.log('New message: ', result);
+  });
 
-   // Get users
-   let users = await api.getUsers({limit: 100});
-   console.log('List of all users', users);
+    //
+    // Actions
+    //
 
-   // We have more users
-   if (users.next) {
-       console.log('More users', await api.getUsers(users.next));
-   }
+    // Fetch current user
+    const user = await api.getCurrentUser();
+    console.log('Current user', user);
 
-   // Create room
-   try {
-       console.log('New room created', await api.createRoom({name: 'Test'}));
-   } catch (err) {
-       console.log(err.message);
-   }
+    // Fetch user information
+    console.log('User information', await api.getUser(user._id));
 
-   // Get list of all rooms
-   let rooms = await api.getRooms();
-   console.log('All rooms', rooms);
+    // Get users
+    const users = await api.getUsers({ limit: 100 });
+    console.log('List of all users', users);
 
-   console.log('Get room info', await api.getRoom(rooms.items[0]._id));
+    // We have more users
+    if (users.next) {
+        console.log('More users', await api.getUsers(users.next));
+    }
 
-   // Try to join to first room in list
-   console.log('Join current user to room', await api.currentUserJoinRoom(rooms.items[0]._id));
+    // Create room
+    try {
+        console.log('New room created', await api.createRoom({ name: 'Test' }));
+    } catch (err) {
+        console.log(err.message);
+    }
 
-   // Try to join to first room in list
-   console.log('Join some user to room', await api.userJoinRoom(users.items[0]._id, rooms.items[0]._id));
+    // Get list of all rooms
+    const rooms = await api.getRooms();
+    console.log('All rooms', rooms);
 
-   // Get current user list of rooms
-   console.log('Current user rooms: ', await api.getCurrentUserRooms());
+    console.log('Get room info', await api.getRoom(rooms.items[0]._id));
 
-   // Send message to room
-   console.log('Send message', await api.sendMessage(rooms.items[0]._id, `Test message ${Date.now()}`));
+    // Try to join to first room in list
+    console.log('Join current user to room', await api.currentUserJoinRoom(rooms.items[0]._id));
 
-   // Send message to room
-   console.log('Room messages', await api.getRoomMessages(rooms.items[0]._id));
+    // Try to join to first room in list
+    console.log('Join some user to room', await api.userJoinRoom(users.items[0]._id, rooms.items[0]._id));
 
+    // Get current user list of rooms
+    console.log('Current user rooms: ', await api.getCurrentUserRooms());
+
+    // Send message to room
+    console.log('Send message', await api.sendMessage(rooms.items[0]._id, `Test message ${Date.now()}`));
+
+    // Send message to room
+    console.log('Room messages', await api.getRoomMessages(rooms.items[0]._id));
+
+    // Leave room
+    console.log('Leave current user to room', await api.currentUserLeaveRoom(rooms.items[0]._id));
    // Leave room
    console.log('Leave current user to room', await api.currentUserLeaveRoom(rooms.items[0]._id));
 
-   console.log(api);
+    console.log(api);
 })();
 
-//  const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducer,
@@ -119,4 +141,5 @@ ReactDOM.render(
   </Provider>
   , document.getElementById('root')
 );
+
 registerServiceWorker();
